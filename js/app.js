5cdc61df1580c690fac16d91c1f8ac67fa71900a
Özgür TÜZGEN <otuzgen@stm.com.tr>
2017-09-23 15:30:08 +0300

    same functionality with seperated functions

--- js/app.js
+++ js/app.js
@@ -2,27 +2,24 @@ $(document).ready(function () {
     let cardList = ["fa-diamond", "fa-paper-plane-o", "fa-anchor", "fa-bolt", "fa-cube", "fa-leaf", "fa-bicycle", "fa-bomb",
         "fa-diamond", "fa-paper-plane-o", "fa-anchor", "fa-bolt", "fa-cube", "fa-leaf", "fa-bicycle", "fa-bomb"];
 
-    let lastOpenedCard;
-    let foundCardCount;
     let moveCount;
+    let openCards = [];
 
+    var Card = function (id, className) {
+        this.id = id;
+        this.className = className;
+    };
 
     function initiateGame() {
-        let container = $("#deck");        
-        container.empty();        
+        let container = $("#deck");
+        container.empty();
 
-        moveCount = 0;
-        lastOpenedCard = null;
-        foundCardCount = 0;
-        cardList = shuffle(cardList);    
+        resetMoveCount(0);
+        resetStars();
+
+        cardList = shuffle(cardList);
         initiateCards(cardList);
     }
-
-    initiateGame();    
-
-    $("#restartButton").click(function () {
-        initiateGame();        
-    });
 
     function initiateCards(array) {
         let container = $("#deck");
@@ -35,43 +32,63 @@ $(document).ready(function () {
             li.append(i);
 
             li.click(function () {
-                li.addClass("show"); //show first
 
-                if (lastOpenedCard === null) {
-                    lastOpenedCard = li[0];
+                displayCard(li);
+
+                var card = new Card(li[0].id, li.find("i")[0].className);
+
+                addCardToList(card);
+
+                if (openCards.length % 2 === 1) {
+                    return;
+                }
+
+                var card1 = openCards[openCards.length - 2];
+                var card2 = openCards[openCards.length - 1];
+
+                if ((card1.className === card2.className) && (card1.id === card2.id)) {
+                    return;
+                }
+
+                if (card1.className === card2.className) {
+                    setCardsAsMatch(card1, card2);                    
                 }
                 else {
-                    let foundI = li.find("i")[0];
-                    let lastI = lastOpenedCard.children[0];
-
-                    //compare for match
-                    if (foundI.className == lastI.className) {
-                        // same item is clicked
-                        if (li[0].id == lastOpenedCard.id) {
-                            return;
-                        }
-
-                        li.removeClass("show");
-                        li.addClass("match");
-                        $("#" + lastOpenedCard.id).removeClass("show");
-                        $("#" + lastOpenedCard.id).addClass("match");
-                        lastOpenedCard = null;
-                        foundCardCount = foundCardCount + 2;
-                        checkGameEnding();
-                    }
-                    else {
-                        setTimeout(function () {
-                            $("#" + lastOpenedCard.id).removeClass("show");
-                            li.removeClass("show");
-                            lastOpenedCard = null;
-                        }, 200);
-                        incrementMove();
-                        checkGameScore();
-                    }
+                    setCardsNotMatched(card1, card2);
+                    
+                    incrementMove();
+                    checkGameScore();
                 }
+
             });
             container.append(li);
         }
+    }
+
+    function displayCard(card) {
+        card.addClass("show"); //show first
+    }
+
+    function addCardToList(card) {
+        openCards.push(card);
+    }
+
+    function setCardsAsMatch(card1, card2) {
+        $("#" + card1.id).removeClass("show");
+        $("#" + card1.id).addClass("match");
+        $("#" + card2.id).removeClass("show");
+        $("#" + card2.id).addClass("match");
+
+        checkGameEnding();
+    }
+
+    function setCardsNotMatched(card1, card2) {
+        setTimeout(function () {
+            $("#" + card1.id).removeClass("show");
+            openCards.splice(-1, 1)
+            $("#" + card2.id).removeClass("show");
+            openCards.splice(-1, 1)            
+        }, 200);
     }
 
     function incrementMove() {
@@ -81,25 +98,41 @@ $(document).ready(function () {
     }
 
     function checkGameEnding() {
-        if (foundCardCount == cardList.length) {
+        if (openCards.length == cardList.length) {
             alert("The game ended with a score: " + moveCount);
         }
     }
 
-    function checkGameScore() {        
-        if (moveCount===15){
+    function checkGameScore() {
+        if (moveCount === 15) {
             removeStar();
         }
-        if (moveCount===10){
+        if (moveCount === 10) {
             removeStar();
         }
-        else if (moveCount===5){
+        else if (moveCount === 5) {
             removeStar();
+        }
+    }
+
+    function resetStars() {
+        var stars = $("#stars");
+        while (stars[0].children.length < 3) {
+            let li = $("<li>");
+            let i = $("<i>", { "class" : "fa fa-star" });
+            
+            li.append(i);
+            stars.append(li);
         }
     }
 
     function removeStar() {
         $('#stars li:last').remove();
+    }
+
+    function resetMoveCount(count) {
+        moveCount = count;
+        $("#moveCounter").text(count);
     }
 
     // Shuffle function from http://stackoverflow.com/a/2450976
@@ -117,15 +150,9 @@ $(document).ready(function () {
         return array;
     }
 
-    /*
-     * set up the event listener for a card. If a card is clicked:
-     *  - display the card's symbol (put this functionality in another function that you call from this one)
-     *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
-     *  - if the list already has another card, check to see if the two cards match
-     *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
-     *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
-     *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
-     *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
-     */
+    initiateGame();
 
+    $("#restartButton").click(function () {
+        initiateGame();
+    });
 });
